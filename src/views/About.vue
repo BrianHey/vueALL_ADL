0e6796ea12dbdc4eebc6a903ffa579927f65f85d
Viviana <vivi_reyes_c@hotmail.com>
2020-09-09 21:58:27 -0300

    mostrar datos de la api desde About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-for='(item, indice) in items' :key="indice">
+      <img :src="item.links[0].href" width="100" alt="">
+      <h6>{{item.data[0].title}}</h6>
+      <p>{{item.data[0].description}}</p>
+
+    </div>
   </div>
 </template>
+<script>
+export default {
+  computed: {
+    items(){
+      return this.$store.state.data.collection.items
+    } 
+  },
+ 
+}
+</script>
