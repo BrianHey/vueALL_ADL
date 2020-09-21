89977064d7638675a264334f2be22739153f1b6d
Brian <brian.habib@brandnlabel.com>
2020-09-21 19:19:07 -0300

    reset total

--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,8 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    data: {}
-  },
-  mutations: {
-    CONSUMIR_API(state, data){
-      state.data = data
-    }
-  },
-  actions: {
-    consumirApi(store){
-      fetch('https://images-api.nasa.gov/search?q=venus')
-      .then(res => res.json())
-      .then(data => store.commit('CONSUMIR_API', data))},
-  },
-  modules: {
-  }
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
 })
