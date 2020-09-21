2fb8b8e124c9ff7a9122df51e66ff9e67852d7c8
Natalia <naty.jimenez@gmail.com>
2020-09-21 20:32:43 -0300

    Inventario

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    juguetes: [
+      { id:"001", nombre: "superman", stock: 100, precio: 5000},
+      { id:"002", nombre: "batman", stock: 300, precio: 7000},
+      { id:"003", nombre: "iron-man", stock: 500, precio: 9000},
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {},
--- src/views/Inventario.vue
+++ src/views/Inventario.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>Inventario</h1>
-    </div>
+  <div>
+      <h1>Inventario</h1>
+    <b-table striped hover :items="juguetes" :fields="fields"></b-table>
+  </div>
 </template>
+
+<script>
+  import {mapState} from "vuex"
+  export default {
+    data() {
+      return {
+        fields: ['id', 'nombre', 'stock', 'precio'],
+      }
+    },
+    computed: {
+        ...mapState([
+            "juguetes"
+
+        ])
+    }
+  }
+</script>
