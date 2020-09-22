c517672967c29af1429c79780ba2ac225a92da37
Brian <brian.habib@brandnlabel.com>
2020-09-21 22:01:49 -0300

    logica extra

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,29 +1,43 @@
 <template>
-	<div class="home">
-    <label for=""> Id </label>
-		<b-form-input id="input-2" v-model="id"  placeholder="Nombre"></b-form-input>
-		<label for=""> Nombre </label>
-		<b-form-input id="input-2"  placeholder="Nombre"></b-form-input>
-		<label for=""> Stock </label>
-		<b-form-input id="input-2"  placeholder="Stock"></b-form-input>
-		<label for=""> Precio </label>
-		<b-form-input id="input-2" placeholder="Precio"></b-form-input>
-		<b-button type="submit" @click="Descontar" variant="primary">Comprar</b-button>
-	</div>
+<div class="home container px-5">
+  <label for>Id</label>
+  <b-form-input id="input-2" v-model="id" placeholder="Nombre"></b-form-input>
+  <label for>Nombre</label>
+  <b-form-input :disabled="true" id="input-2" v-model="juguete.nombre" placeholder="Nombre"></b-form-input>
+  <label for>Stock</label>
+  <b-form-input :disabled="true" id="input-2" v-model="juguete.stock" placeholder="Stock"></b-form-input>
+  <label for>Precio</label>
+  <b-form-input :disabled="true" id="input-2" v-model="juguete.precio" placeholder="Precio"></b-form-input>
+  <b-button type="submit" @click="Descontar" variant="primary">Comprar</b-button>
+</div>
 </template>
 
 <script>
+import {
+  mapState
+} from "vuex";
 export default {
-  name: 'Home',
+  name: "Home",
   methods: {
     Descontar() {
-       this.$store.dispatch("descontar" , this.id)
+      this.$store.dispatch("descontar", this.id);
     },
   },
-  data (){
+  data() {
     return {
-      id : ""
-    }
-  }
+      id: "",
+    };
+  },
+  computed: {
+    ...mapState(["juguetes"]),
+    juguete() {
+      let juguete = {
+        nombre: "",
+        precio: "",
+        stock: "",
+      };
+      return this.juguetes.find((j) => j.id == this.id) || juguete;
+    },
+  },
 };
 </script>
